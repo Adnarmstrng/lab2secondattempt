4b085515a13cdc8eda0c76b66525601a24ac6b9f
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-02-12 21:10:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ map.on('load', () => {
     // Add a data source from a GeoJSON file
     map.addSource('parks', {
         type: 'geojson',
-        data: 'https://raw.githubusercontent.com/Adnarmstrng/lab2secondattempt/main/parks.geojson' 
-        
+        data: 'https://raw.githubusercontent.com/Adnarmstrng/lab2secondattempt/main/parks.geojson'
+
     });
     map.addLayer({
         'id': 'parks-point',
@@ -24,23 +24,23 @@ map.on('load', () => {
     })
 
     // Add a data source from a Mapbox tileset
-map.addSource('censustracts', { // Create your own source ID
-    'type': 'vector',
-    'url': 'mapbox://aidanarmstrong.a2ueu202' // Update to your mapbox tileset ID
+    map.addSource('censustracts', { // Create your own source ID
+        'type': 'vector',
+        'url': 'mapbox://aidanarmstrong.a2ueu202' // Update to your mapbox tileset ID
     });
 
     map.addLayer({
-    'id': 'census', // Create your own layer ID
-    'type': 'fill', // Note this is different to point data
-    'source': 'censustracts', // Must match source ID from addSource Method
-    'paint': {
-    'fill-color': '#888888', // Test alternative colours and style properties
-    'fill-opacity': 0.4,
-    'fill-outline-color': 'black'
+        'id': 'census', // Create your own layer ID
+        'type': 'fill', // Note this is different to point data
+        'source': 'censustracts', // Must match source ID from addSource Method
+        'source-layer': 'torontoct-43nn0m', // Tileset NAME (diff to ID), get this from mapbox
+        'paint': {
+            'fill-color': '#888888', // Test alternative colours and style properties
+            'fill-opacity': 0.4,
+            'fill-outline-color': 'black'
+        }
+
     },
-    'source-layer': 'torontoct-43nn0m' // Tileset NAME (diff to ID), get this from mapbox
-  
-    },
-    'parks'
+        'parks'
     );
-})
+});
