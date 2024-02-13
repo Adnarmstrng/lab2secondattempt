925764d0cea7fc70bf789595bf7ce7536e9c0f43
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-02-12 21:34:22 -0500

    debugging

--- script.js
+++ script.js
@@ -6,93 +6,39 @@ const map = new mapboxgl.Map({
     zoom: 9 // starting zoom
 });
 
-map.on('load', () => {
-    // Add a data source from a GeoJSON file
-    map.addSource('parks', {
-        type: 'geojson',
-        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data: 'https://raw.githubusercontent.com/Adnarmstrng/lab2secondattempt/main/parks.geojson',
-        cluster: true,
-        clusterMaxZoom: 12, // Max zoom to cluster points on
-        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-    });
-
-    map.addLayer({
-        id: 'clusters',
-        type: 'circle',
-        source: 'parks',
-        filter: ['has', 'point_count'],
-        paint: {
-            // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
-            // with three steps to implement three types of circles:
-            //   * Blue, 20px circles when point count is less than 100
-            //   * Yellow, 30px circles when point count is between 100 and 750
-            //   * Pink, 40px circles when point count is greater than or equal to 750
-            'circle-color': [
-                'step',
-                ['get', 'point_count'],
-                '#51bbd6',
-                20,
-                '#f1f075',
-                50,
-                '#f28cb1'
-            ],
-            'circle-radius': [
-                'step',
-                ['get', 'point_count'],
-                20,
-                100,
-                30,
-                750,
-                40
-            ]
-        }
-    });
-
-    map.addLayer({
-        id: 'cluster-count',
-        type: 'symbol',
-        source: 'parks',
-        filter: ['has', 'point_count'],
-        layout: {
-            'text-field': ['get', 'point_count_abbreviated'],
-            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-            'text-size': 12
-        }
-    });
-
-    map.addLayer({
-        id: 'unclustered-point',
-        type: 'circle',
-        source: 'parks',
-        filter: ['!', ['has', 'point_count']],
-        paint: {
-            'circle-color': '#11b4da',
-            'circle-radius': 4,
-            'circle-stroke-width': 1,
-            'circle-stroke-color': '#fff'
-        }
-    });
-
-    // Add a data source from a Mapbox tileset
-    map.addSource('censustracts', { // Create your own source ID
-        'type': 'vector',
-        'url': 'mapbox://aidanarmstrong.a2ueu202' // Update to your mapbox tileset ID
-    });
-
-    map.addLayer({
-        'id': 'census', // Create your own layer ID
-        'type': 'fill', // Note this is different to point data
-        'source': 'censustracts', // Must match source ID from addSource Method
-        'source-layer': 'torontoct-43nn0m', // Tileset NAME (diff to ID), get this from mapbox
-        'paint': {
-            'fill-color': '#888888', // Test alternative colours and style properties
-            'fill-opacity': 0.4,
-            'fill-outline-color': 'black'
-        }
-
-    },
-
-    );
+// Add a data source from a GeoJSON file
+map.addSource('parks', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/Adnarmstrng/lab2secondattempt/main/parks.geojson'
+    // Your URL to your buildings.geojson file
 });
+map.addLayer({
+    'id': 'parks-point',
+    'type': 'circle',
+    'source': 'parks',
+    'paint': {
+        'circle-radius': 5,
+        'circle-color': '#007cbf'
+    }
+});
+
+// Add a data source from a Mapbox tileset
+map.addSource('censustracts', { // Create your own source ID
+    'type': 'vector',
+    'url': 'mapbox://aidanarmstrong.a2ueu202' // Update to your mapbox tileset ID
+});
+
+map.addLayer({
+    'id': 'census', // Create your own layer ID
+    'type': 'fill', // Note this is different to point data
+    'source': 'censustracts', // Must match source ID from addSource Method
+    'source-layer': 'torontoct-43nn0m', // Tileset NAME (diff to ID), get this from mapbox
+    'paint': {
+        'fill-color': '#888888', // Test alternative colours and style properties
+        'fill-opacity': 0.4,
+        'fill-outline-color': 'black'
+    }
+
+},
+
+);
